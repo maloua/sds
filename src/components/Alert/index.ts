a75498872283967e57b1ac338590d8a702b1c274
maloua <maloua@users.noreply.github.com>
2024-03-14 10:52:08 +0100

    convert alert to wc

--- src/components/Alert/index.ts
+++ src/components/Alert/index.ts
@@ -1,2 +1,7 @@
-export {default as Alert} from "./Alert";
-export {default as AlertType} from "./AlertType";
+import r2wc from "@r2wc/react-to-web-component"
+import { default as AlertReact } from "./Alert";
+
+export const Alert = r2wc(AlertReact, { props: ["message"] });
+// customElements.define("sds-alert", Alert);
+
+export { default as AlertType } from "./AlertType";
